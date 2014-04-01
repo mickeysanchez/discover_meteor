5a105e295469bff90bb2dae2eb61f214fd644d78
Mickey <sanchez.mickey@gmail.com>
2014-04-01 17:16:11 -0400

    finishes up to 10.5

---
+++ collections/comments.js
@@ -0,0 +1,30 @@
+Comments = new Meteor.Collection('comments');
+
+Meteor.methods({
+	comment: function(commentAttributes) {
+		var user = Meteor.user();
+		var post = Posts.findOne(commentAttributes.postId);
+		
+		if (!user) {
+			throw new Meteor.Error(401, "You need ta log in.");
+		};
+		
+		if (!commentAttributes.body) {
+			throw new Meteor.Error(402, "Needs body.");
+		};
+		
+		if (!post) {
+			throw new Meteor.Error(402, "Gotta comment on a post.");
+		};
+		
+		comment = _.extend(_.pick(commentAttributes, 'postId', 'body'), {
+			userId: user._id,
+			author: user.username,
+			submitted: new Date().getTime()
+		});
+		
+		Posts.update(comment.postId, {$inc: {commentsCount: 1}});
+		
+		return Comments.insert(comment);
+	}
+});
--- collections/posts.js
+++ collections/posts.js
@@ -1,5 +1,16 @@
 Posts = new Meteor.Collection('posts');
 
+Posts.allow({ 
+	update: ownsDocument, 
+	remove: ownsDocument
+});
+
+Posts.deny({
+	update: function(userId, post, fieldNames) {
+		return (_.without(fieldNames, 'url', 'title').length > 0);
+	}
+});
+
 Meteor.methods({
 	post: function(postAttributes) {
 		var user = Meteor.user();
@@ -20,7 +31,8 @@ Meteor.methods({
 		var post = _.extend(_.pick(postAttributes, "url", "title", "message"), {
 			userId: user._id,
 			author: user.username,
-			submitted: new Date().getTime()
+			submitted: new Date().getTime(),
+			commentsCount: 0
 		});
 		
 		var postId = Posts.insert(post);
