143c605c7bb3fb492fbef1626a547bae2e1fb337
Mickey <sanchez.mickey@gmail.com>
2014-03-31 21:46:30 -0400

    finishes up to 7.5 latency compensation

--- collections/posts.js
+++ collections/posts.js
@@ -1,1 +1,30 @@
 Posts = new Meteor.Collection('posts');
+
+Meteor.methods({
+	post: function(postAttributes) {
+		var user = Meteor.user();
+		var postWithSameLink = Posts.findOne({ url: postAttributes.url });
+			
+		if (!user) {
+			throw new Meteor.Error(401, "You need to login to post new stories.");
+		};
+		
+		if (!postAttributes.title) {
+			throw new Meteor.Error(422, "We need a title baby.");
+		};
+		
+		if (postAttributes.url && postWithSameLink) {
+			throw new Meteor.Error(302, "Link already posted!", postWithSameLink._id);
+		};
+		
+		var post = _.extend(_.pick(postAttributes, "url", "title", "message"), {
+			userId: user._id,
+			author: user.username,
+			submitted: new Date().getTime()
+		});
+		
+		var postId = Posts.insert(post);
+		
+		return postId;
+	}
+});
