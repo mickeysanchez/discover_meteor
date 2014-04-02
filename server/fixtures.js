7cca9a530113636aa626929da4e9786119e0d6ca
Mickey <sanchez.mickey@gmail.com>
2014-04-02 18:29:06 -0400

    finishes up to 13 'voting'

--- server/fixtures.js
+++ server/fixtures.js
@@ -54,4 +54,15 @@ if (Posts.find().count() === 0) {
     submitted: now - 12 * 3600 * 1000,
 	commentsCount: 0
   });
-}
+  
+  for (var i = 0; i < 10; i++) {
+	  Posts.insert({
+		  title: 'Test post #' + i,
+		  author: sacha.profile.name,
+		  userId: sacha._id,
+		  url: i,
+		  submitted: now-1 * 3600 * 1000,
+		  commentsCount: 0
+	  });
+  };
+};
